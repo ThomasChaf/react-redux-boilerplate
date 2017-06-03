0aeb8ddd9fda9b43f592c51ae9706e3a2faaeb95
Thomas Chafiol <thomaschaf@gmail.com>
2017-06-03 18:33:53 +0200

    Add minimal configuration + first route

---
+++ .next/2.0ae6417e423b585df770.hot-update.js
@@ -0,0 +1,50 @@
+webpackHotUpdate(2,{
+
+/***/ 147:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _react = __webpack_require__(141);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var HomePageComponent = function HomePageComponent() {
+	  return _react2.default.createElement(
+	    'div',
+	    null,
+	    'Hello world'
+	  );
+	};
+	
+	exports.default = HomePageComponent;
+	//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkhvbWVQYWdlQ29tcG9uZW50Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTs7Ozs7O0FBRUEsSUFBTUEsb0JBQW9CLFNBQXBCQSxpQkFBb0IsR0FBTTtBQUM5QixTQUNFO0FBQUE7QUFBQTtBQUFBO0FBQUEsR0FERjtBQUdELENBSkQ7O2tCQU1lQSxpQiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvY2hhZmlvbC9Eb2N1bWVudHMvRXBpdGVjaC9Cb2lsZXJwbGF0ZSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcblxuY29uc3QgSG9tZVBhZ2VDb21wb25lbnQgPSAoKSA9PiB7XG4gIHJldHVybiAoXG4gICAgPGRpdj5IZWxsbyB3b3JsZDwvZGl2PlxuICApXG59XG5cbmV4cG9ydCBkZWZhdWx0IEhvbWVQYWdlQ29tcG9uZW50XG4iXX0=
+	    if (true) {
+	      module.hot.accept()
+
+	      var Component = module.exports.default || module.exports
+	      Component.__route = "/"
+
+	      if (module.hot.status() !== 'idle') {
+	        var components = next.router.components
+	        for (var r in components) {
+	          if (!components.hasOwnProperty(r)) continue
+
+	          if (components[r].Component.__route === "/") {
+	            next.router.update(r, Component)
+	          }
+	        }
+	      }
+	    }
+	  
+
+/***/ }
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWdlcy9pbmRleC5qcz9mN2VlIl0sIm5hbWVzIjpbIkhvbWVQYWdlQ29tcG9uZW50Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7OztBQUFBOzs7Ozs7QUFFQSxLQUFNQSxvQkFBb0IsU0FBcEJBLGlCQUFvQixHQUFNO0FBQzlCLFVBQ0U7QUFBQTtBQUFBO0FBQUE7QUFBQSxJQURGO0FBR0QsRUFKRDs7bUJBTWVBLGlCIiwiZmlsZSI6IjIuMGFlNjQxN2U0MjNiNTg1ZGY3NzAuaG90LXVwZGF0ZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcblxuY29uc3QgSG9tZVBhZ2VDb21wb25lbnQgPSAoKSA9PiB7XG4gIHJldHVybiAoXG4gICAgPGRpdj5IZWxsbyB3b3JsZDwvZGl2PlxuICApXG59XG5cbmV4cG9ydCBkZWZhdWx0IEhvbWVQYWdlQ29tcG9uZW50XG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9wYWdlcy9pbmRleC5qcyJdLCJzb3VyY2VSb290IjoiIn0=
