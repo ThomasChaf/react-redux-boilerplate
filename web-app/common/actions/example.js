f12ab0492dce5599feec0376442fd462b83f96d8
Thomas Chafiol <thomaschaf@gmail.com>
2017-06-18 22:09:48 +0200

    Remove useless next folder

--- web-app/common/actions/example.js
+++ web-app/common/actions/example.js
@@ -2,10 +2,7 @@ import * as actionTypes from '../actionTypes'
 import api from '../api'
 
 export const action_1 = () => async (dispatch, getState) => {
-  console.log('JE FAIS ACTION 1')
-  console.log(api)
   const res = await api.get('/action_1')
-  console.log('JAI PAS DE RETOUR API')
   dispatch({
     type: actionTypes.ACTION_EXAMPLE_1,
     payload: { ...res }
--- web-app/components/example_1/container.js
+++ web-app/components/example_1/container.js
@@ -2,22 +2,13 @@ import { connect } from 'react-redux'
 import { actions } from 'common'
 import Component from './component'
 
-const mapStateToProps = (state) => {
-  console.log('======= STATE ======');
-  console.log(state)
-  return {
-    clicked: state.example.example_1.clicked
-  }
-}
+const mapStateToProps = (state) => ({
+  clicked: state.example.example_1.clicked
+})
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    action_1: () => {
-      console.log('JE CALL EXEMPLA AC 1');
-      dispatch(actions.example.action_1())
-    }
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  action_1: () => dispatch(actions.example.action_1())
+})
 
 export default connect(
   mapStateToProps,
