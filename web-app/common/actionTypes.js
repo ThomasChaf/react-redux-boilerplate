bc6cf78a309854569adf24c82d0ab14b431d640d
Thomas Chafiol <thomaschaf@gmail.com>
2017-06-19 01:39:50 +0200

    Add global config for having a special common dir

--- web-app/common/actionTypes.js
+++
@@ -1,3 +0,0 @@
-/** Example actions **/
-export const ACTION_EXAMPLE_1 = 'example_1'
-export const ACTION_EXAMPLE_2 = 'example_2'
--- web-app/common/index.js
+++
@@ -1,2 +0,0 @@
-export * as  actions from './actions'
-export { default as store } from './store'
