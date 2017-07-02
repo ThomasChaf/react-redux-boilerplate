75552a6df88ee1d41581b4e56d303d0171fece98
Thomas Chafiol <thomaschaf@gmail.com>
2017-07-03 00:42:03 +0200

    Fix config and usage of commons

--- common/build/build/actionTypes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-/** Example actions **/
-var ACTION_EXAMPLE_1 = exports.ACTION_EXAMPLE_1 = 'example_1';
-var ACTION_EXAMPLE_2 = exports.ACTION_EXAMPLE_2 = 'example_2';
