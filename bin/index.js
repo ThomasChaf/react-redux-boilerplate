a6e3817f0afea19e812865de48e107a92de2605c
Thomas Chafiol <thomaschaf@gmail.com>
2017-07-16 22:32:12 +0200

    Finish script to install boilerplate.

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,44 @@
 #!/usr/bin/env node
 
-console.log("OK CA MARCHE")
+var fs = require('fs')
+var path = require('path')
+
+if (process.argv.length !== 3 || process.argv[2] === "-h" || process.argv[2] === "--help") {
+  console.log("Usage: react-redux-native-boilerplate [APP NAME]")
+  process.exit(0)
+}
+
+if (process.argv[2] === "-v") {
+  console.log("v 1.4.0")
+  process.exit(0)
+}
+
+var dir = path.join(process.cwd(), '/', process.argv[2])
+console.log('Create:', dir)
+
+if (fs.existsSync(dir)) {
+  console.log('Error directory already exist')
+  process.exit(0)
+}
+
+fs.mkdirSync(dir)
+
+var copy = function(src, dest) {
+  var exists = fs.existsSync(src)
+  var stats = exists && fs.statSync(src)
+  var isDirectory = exists && stats.isDirectory()
+  if (exists && isDirectory) {
+    fs.mkdirSync(dest)
+    fs.readdirSync(src).forEach(function(childItemName) {
+      copy(path.join(src, childItemName), path.join(dest, childItemName))
+    })
+  } else if (exists) {
+    fs.createReadStream(src).pipe(fs.createWriteStream(dest))
+  }
+}
+
+var dirnNames = ['api', 'common', 'mobile-apps', 'scripts', 'web-app']
+
+dirnNames.forEach(function(dirName) {
+  copy(path.join(__dirname, '..', dirName), path.join(dir, dirName))
+})
